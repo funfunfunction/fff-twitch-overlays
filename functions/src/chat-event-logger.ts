af51516bce3bd2190eb3518fa80d550bb1328fb8
Mattias Petter Johansson <mpj@mpj.me>
2020-05-01 12:50:28 +0200

    Workt1431 (#18)
    * added reconnect and secure param to tmi
    
    * package.json update
    
    * extracted chat-event-logger
    
    * remove brainstormy comment
    
    * tmiRaw view
    
    * fix accidental async function
    
    * move comment for clarification, add nan check
    
    * style
    
    * fix state collection ref
    
    * fix another state refence
    
    * style
    
    * inlined db ref
    
    * style

--- functions/src/chat-event-logger.ts
+++ functions/src/chat-event-logger.ts
@@ -1,154 +1,65 @@
-import * as functions from "firebase-functions"
 import * as admin from "firebase-admin"
 
 import getOwnerUsername from "./helpers/assorted/get-owner-username"
 import getOwnerAccessToken from "./helpers/assorted/get-owner-access-token"
-import getChannelOwnerUserId from "./helpers/assorted/get-channel-owner-user-id"
-
-import { getStreams } from "./helpers/twitch"
 
 import tmi from "tmi.js"
+import whileTwitchLive from "./helpers/assorted/while-twitch-live"
 
 const db = admin.firestore()
 
-const TWITCH_CLIENT_ID = functions.config().twitch.client_id
-
-// TODO can we get this to trigger on twitch-live-status updates by adding some lastupate,
-// (but be beware of triggering tons of updates)
-// not sure if we can combine with runWith as we need the timeoutSeconds
-
-const chatEventLogger = functions
-  .runWith({
-    memory: "128MB",
-    timeoutSeconds: 9 * 60 // max allowed run time for cloud functions is 9 min
-  })
-  .pubsub.schedule("every 1 minutes")
-  .onRun(async context => {
-    // Only start logger if we're actually live on Twitch
-    const ownerAccessToken = await getOwnerAccessToken()
-    if (!(await isStreaming(ownerAccessToken))) {
-      console.log("FFF is not live, no need to start up any logger")
-      return null
+const chatEventLogger = whileTwitchLive("chat-event-logger", async function() {
+  const accessToken = await getOwnerAccessToken()
+  if (!accessToken) {
+    console.error("Could not get owner access token, shutting down.")
+    process.exit(1)
+  }
+  const chat = tmi.Client({
+    connection: {
+      reconnect: true,
+      secure: true
+    },
+    identity: {
+      username: getOwnerUsername(),
+      password: "oauth:" + accessToken
     }
-
-    // As we want reliable logging of chat, we'll use a firestore
-    // document for each run of chatEventLogger to track run state
-    // by sending pulses every few seconds. If we have not
-    // received live signs from an eventLogger for 25000 seconds,
-    // it is assumed to have died and we'll start up some more
-    // for redundancy
-    const assumedDeadTime = Number(Date.now()) - 25000
-    const snapshot = await db
-      .collection("event-logger-state")
-      .where("pulse", ">", assumedDeadTime)
-      .get()
-
-    if (snapshot.size > 1) {
-      console.log(
-        "Got " +
-          snapshot.size +
-          " chatEventLoggers running already, wont start another"
-      )
-      return null
-    } else {
-      console.log(
-        "Detected " +
-          snapshot.size +
-          " chatEventLoggers running, starting one instance now."
-      )
-    }
-
-    // Clean up any state of dead chatEventLoggers
-    await db
-      .collection("event-logger-state")
-      .where("pulse", "<", assumedDeadTime)
-      .get()
-      .then(querySnapshot => querySnapshot.forEach(doc => doc.ref.delete()))
-
-    // Start watching chat
-    ;(async function() {
-      const accessToken = await getOwnerAccessToken()
-      if (!accessToken) {
-        console.error("Could not get owner access token, shutting down.")
-        process.exit(1)
-      }
-      const chat = tmi.Client({
-        identity: {
-          username: getOwnerUsername(),
-          password: "oauth:" + accessToken
-        }
-      })
-      await chat.connect()
-      await chat.join(getOwnerUsername())
-      console.log("chatEventLogger joined channel of ", getOwnerUsername())
-
-      chat.on("chat", (channel, userstate, message) =>
-        logEvent("chat", userstate, { message })
-      )
-
-      chat.on("subscription", (channel, username, method, message, userstate) =>
-        logEvent("subscription", userstate, { method, message })
-      )
-
-      chat.on(
-        "resub",
-        (channel, username, months, message, userstate, methods) =>
-          logEvent("resub", userstate, { months, message, methods })
-      )
-
-      chat.on(
-        "subgift",
-        (channel, username, streakMonths, recipient, methods, userstate) =>
-          logEvent("subgift", userstate, { methods, recipient, streakMonths })
-      )
-
-      chat.on(
-        "submysterygift",
-        async (channel, username, numbOfSubs, methods, userstate) =>
-          logEvent("submysterygift", userstate, { numbOfSubs, methods })
-      )
-
-      chat.on("cheer", async (channel, userstate, message) =>
-        logEvent("cheer", userstate, { message })
-      )
-
-      chat.on("action", async (channel, userstate, message) =>
-        logEvent("action", userstate, { message })
-      )
-    })().catch(async error => {
-      console.error(error)
-      process.exit(1)
-    })
-
-    // pulse every five seconds
-    const intervalId = setInterval(async function() {
-      await db
-        .collection("event-logger-state")
-        .doc(context.eventId)
-        .set({
-          uid: context.eventId,
-          pulse: Number(Date.now())
-        })
-    }, 5000)
-
-    // automatically die gracefully after 8 min, cloud functions will time out after 9 otherwise
-    return new Promise(resolve => {
-      setTimeout(function() {
-        console.log("Closing down chatEventLogger (lifespan ended)")
-        clearInterval(intervalId)
-        resolve()
-      }, 8 * 60 * 1000)
-    })
   })
+  await chat.connect()
+  await chat.join(getOwnerUsername())
+  console.log("chatEventLogger joined channel of ", getOwnerUsername())
 
-async function isStreaming(ownerAccessToken) {
-  const streams = await getStreams(
-    TWITCH_CLIENT_ID,
-    ownerAccessToken,
-    getChannelOwnerUserId()
+  chat.on("chat", (channel, userstate, message) =>
+    logEvent("chat", userstate, { message })
   )
-  return streams.data.length > 0
-}
+
+  chat.on("subscription", (channel, username, method, message, userstate) =>
+    logEvent("subscription", userstate, { method, message })
+  )
+
+  chat.on("resub", (channel, username, months, message, userstate, methods) =>
+    logEvent("resub", userstate, { months, message, methods })
+  )
+
+  chat.on(
+    "subgift",
+    (channel, username, streakMonths, recipient, methods, userstate) =>
+      logEvent("subgift", userstate, { methods, recipient, streakMonths })
+  )
+
+  chat.on(
+    "submysterygift",
+    async (channel, username, numbOfSubs, methods, userstate) =>
+      logEvent("submysterygift", userstate, { numbOfSubs, methods })
+  )
+
+  chat.on("cheer", async (channel, userstate, message) =>
+    logEvent("cheer", userstate, { message })
+  )
+
+  chat.on("action", async (channel, userstate, message) =>
+    logEvent("action", userstate, { message })
+  )
+})
 
 async function logEvent(type, userstate, otherProps) {
   console.log("logEvent triggered:", type, userstate, otherProps)
