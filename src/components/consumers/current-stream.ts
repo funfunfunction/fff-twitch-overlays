b0c81d4dbd51e6ae080f7a1da9cf9968fa319f1b
Mattias Johansson <mpj@mpj.me>
2020-05-06 14:37:35 +0200

    more work on sub current stream

--- src/components/consumers/current-stream.ts
+++ src/components/consumers/current-stream.ts
@@ -5,7 +5,7 @@ import {
 type UnsubscribeFunc = () => void
 
 export function subscribeToCurrentStreamId(
-  callback: (streamId: number) => void
+  callback: (streamId: number | null) => void
 ): UnsubscribeFunc {
   const unsubscribe: UnsubscribeFunc = window.firebase
     .firestore()
@@ -14,9 +14,23 @@ export function subscribeToCurrentStreamId(
       const data = snapshot.data()
       if(!data) {
         console.log('No live data')
+        callback(null)
         return
       }
-      if (typeof data.id === 'number') callback(data.id)
+
+      if (data.live !== false) {
+        callback(null)
+        return
+      }
+      
+      // streamId should really be a number in the database,
+      // but not going to fix this now as more stuff relies 
+      // this view
+      if (typeof data.streamId === 'string') {
+        callback(parseInt(data.streamId))
+      } else {
+        console.error('data was wrong', data)
+      }
     })
   return unsubscribe
 }
