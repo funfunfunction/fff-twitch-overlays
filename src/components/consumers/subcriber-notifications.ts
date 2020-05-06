c55efbf99f5aea2e47a4e3eb669e4ecb2f45b013
Mattias Johansson <mpj@mpj.me>
2020-05-06 13:50:36 +0200

    add consumer

---
+++ src/components/consumers/subcriber-notifications.ts
@@ -0,0 +1,34 @@
+import {
+  SubscriberChatNotificationData,
+  isTMISubscriberNotification,
+  notificationsCollectionPath
+} from "@functions/src/views/subscriber-chat-notification/shared"
+
+type UnsubscribeFunc = () => void
+
+export function subscribeToSubscriberNotifications(
+  streamId: number,
+  callback: (notification: SubscriberChatNotificationData) => void
+): UnsubscribeFunc {
+  console.log("path", notificationsCollectionPath(streamId))
+  const unsubscribe: UnsubscribeFunc = window.firebase
+    .firestore()
+    .collection(notificationsCollectionPath(streamId))
+    .onSnapshot(async function(querySnapshot) {
+      for (const change of querySnapshot.docChanges()) {
+        if (change.type !== "added") {
+          console.error(
+            'Got unexpected change type, I only support "added":',
+            change,
+            change.doc.data()
+          )
+          return
+        }
+        const data = change.doc.data()
+        if (!isTMISubscriberNotification(data))
+          throw Error("Data was not in expected format:" + JSON.stringify(data))
+        callback(data)
+      }
+    })
+  return unsubscribe
+}
