30176bd5981026999ef19c1e05e098307d3b9a48
Mattias Johansson <mpj@mpj.me>
2020-05-13 14:39:31 +0200

    add playground and mock functions

---
+++ functions/src/views/subscriber-chat-notification/playground.ts
@@ -0,0 +1,17 @@
+import credentials from "../../firebase-credentials.json"
+import * as admin from "firebase-admin"
+
+admin.initializeApp({
+  credential: admin.credential.cert(credentials as any),
+  databaseURL: "https://fff-twitch-chat-log.firebaseio.com"
+})
+;(async function() {
+  const db = admin.firestore()
+  db.collection("views/tmi-raw/events")
+    .where("type", "==", "resub")
+    .onSnapshot(snapshot => {
+      for (const doc of snapshot.docs) {
+        console.log(doc.data())
+      }
+    })
+})()
--- src/components/consumers/subcriber-notifications.ts
+++ src/components/consumers/subcriber-notifications.ts
@@ -33,3 +33,34 @@ export function subscribeToSubscriberNotifications(
     })
   return unsubscribe
 }
+
+export function subscribeToMockSubscriberNotifications(
+  streamId: number,
+  callback: (notification: SubscriberChatNotificationData) => void
+): UnsubscribeFunc {
+  setTimeout(function() {
+    callback({
+      cumulativeMonths: 4,
+      displayName: "underscorefunk",
+      id: "575b6eaf-a646-4d2e-8c1f-90081687548a",
+      ts: 1588776346532,
+      userId: "25347823",
+      message: "this is a message"
+    })
+  }, 1000)
+
+  setTimeout(function() {
+    callback({
+      cumulativeMonths: 4,
+      displayName: "Thrennenne",
+      id: "bd652a65-8d93-4b29-9b2e-0759e7157d1b",
+      ts: 1588776335709,
+      userId: "59888285",
+      message: null
+    })
+  }, 3000)
+
+  return () => {
+    // im emptty
+  }
+}
